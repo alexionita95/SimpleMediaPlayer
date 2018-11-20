b918f515398a3675937f28f34c2e321e36bd832f
Alex Ionita <ionita.alexandru95@gmail.com>
2018-11-20 23:32:24 +0200

    Firt commit.

---
+++ MusicPlayerImplementation/MusicPlayerImplementation/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MusicPlayerImplementation
+{
+    public partial class Form1 : Form
+    {
+        MusicPlayer player;
+        public Form1()
+        {
+            InitializeComponent();
+            player = new MusicPlayer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(player.State == PlaybackState.Stopped)
+            {
+                if (listBox1.SelectedIndex >= 0)
+                {
+                    player.SelectMedia(listBox1.SelectedIndex);
+                    button1.Text = "Pause";
+                    player.Play();
+                }
+            }
+            if (player.State==PlaybackState.Paused)
+            {
+                button1.Text = "Pause";
+                player.Play();
+            }
+            else
+            {
+                button1.Text = "Play";
+                player.Pause();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "MP3 Files | *.mp3";
+            ofd.Multiselect = true;
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                player.LoadPlaylist(new List<string>(ofd.FileNames));
+                foreach(string song in player.Playlist)
+                {
+                    System.IO.FileInfo info = new System.IO.FileInfo(song);
+                    listBox1.Items.Add(info.Name);
+                }
+            }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                player.SelectMedia(listBox1.SelectedIndex);
+                button1.Text = "Pause";
+                player.Play();
+            }
+        }
+    }
+}
---
+++ MusicPlayerImplementation/MusicPlayerImplementation/MusicPlayer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMPLib;
+
+namespace MusicPlayerImplementation
+{
+    enum PlaybackState
+    {
+        Playing,
+        Paused,
+        Stopped
+    }
+    class MusicPlayer
+    {
+        List<string> playlist;
+        WindowsMediaPlayer player;
+        public PlaybackState State { get; set; }
+        public List<string> Playlist { get { return playlist; } }
+        public MusicPlayer()
+        {
+            playlist = new List<string>();
+            player = new WindowsMediaPlayer();
+            State = PlaybackState.Stopped;
+            
+        }
+        public void LoadPlaylist(List<string> list)
+        {
+            playlist = list;
+        }
+        public void SelectMedia(int index)
+        {
+            string path = playlist.ElementAt(index);
+            player.URL = path;
+        }
+        public void Play()
+        {
+            player.controls.play();
+            State = PlaybackState.Playing;
+        }
+        public void Pause()
+        {
+            player.controls.pause();
+            State = PlaybackState.Paused;
+        }
+        public void Stop()
+        {
+            player.controls.stop();
+            State = PlaybackState.Stopped;
+        }
+    }
+}
